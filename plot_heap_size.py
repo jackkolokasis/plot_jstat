77e115c25a3e0418b3e6ffe68ea6421cb331ead2
Iacovos Kolokasis <kolokasis@ics.forth.gr>
2024-04-19 11:06:20 +0300

    [refactor] Remove duplicate code

--- plot_heap_size.py
+++ plot_heap_size.py
@@ -19,100 +19,102 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import config
 
+##
+# Transform size
+# @param size: The size in killobytes
+#
+# @return The size in gigabytes
 def kb_to_gb(size):
     return float(size)/1024/1024
 
-# Parse input arguments
-usage = "usage: %prog [options]"
-parser = optparse.OptionParser(usage=usage)
-parser.add_option("-i", "--input", dest="input", metavar="FILE", help="Input file")
-parser.add_option("-o", "--outputPath", metavar="PATH", dest="outputPath", default="output.svg", help="Output Path")
-(options, args) = parser.parse_args()
+##
+# Plot data about the capacity of the heap
+# @param time: The time points
+# @param min_data: The minimum capacity of the heap
+# @param max_data: The maximum capacity of the heap
+# @param cur_data: The current capacity of the heap
+# @param label_prefix: The label prefix for the plot
+# @param output_filename: The output filename
+#
+# return None
+##
+def plot_capacity_data(time, min_data, max_data, cur_data, label_prefix, output_filename):
+    fig, ax = plt.subplots(figsize=config.fullfigsize)
 
-young_gen_min = []
-young_gen_max = []
-young_gen_cur = []
+    # Grid
+    plt.grid(True, linestyle='--', color='grey', zorder=0)
 
-old_gen_min = []
-old_gen_max = []
-old_gen_cur = []
+    # Plot data
+    plt.plot(time, min_data, color=config.B_color_cycle[0],
+             label=f'{label_prefix} Minimum Capacity', zorder=2)
+    plt.plot(time, max_data, color=config.B_color_cycle[1],
+             label=f'{label_prefix} Maximum Capacity', zorder=2)
+    plt.plot(time, cur_data, color=config.B_color_cycle[3],
+             label=f'{label_prefix} Current Capacity', zorder=2)
 
-# Open input file 
-with open(options.input, 'r') as inputFile:
-    # Initialize a flag to track whether the first line has been bypassed
-    first_line_skipped = False
+    # Axis labels
+    plt.ylabel('Memory (GB)', ha="center")
+    plt.xlabel('Time (s)')
 
-    # Iterate over each line in the file
-    for line in inputFile.readlines():
-        # Check if it's the first line
-        if not first_line_skipped:
-            # Set the flag to True to indicate that the first line has been bypassed
-            first_line_skipped = True
-            continue  # Skip processing this line and move to the next one
-        
-        # Split the line into individual columns based on whitespace
-        columns = line.strip().split()
+    # Legend
+    plt.legend(loc='upper right', bbox_to_anchor=(1, 1.25), ncol=3)
 
-        young_gen_min.append(kb_to_gb(columns[0]))
-        young_gen_max.append(kb_to_gb(columns[1]))
-        young_gen_cur.append(kb_to_gb(columns[2]))
-        
-        old_gen_min.append(kb_to_gb(columns[6]))
-        old_gen_max.append(kb_to_gb(columns[7]))
-        old_gen_cur.append(kb_to_gb(columns[8]))
+    # Save figure
+    plt.savefig(output_filename, bbox_inches='tight')
 
-#------------------------------------------------------------------------------
-# Young Generation Capacity
-#------------------------------------------------------------------------------
-fig, ax = plt.subplots(figsize=config.fullfigsize)
+##
+# @brief Main function
+#
+# return
+def main():
+    # Parse input arguments
+    usage = "usage: %prog [options]"
+    parser = optparse.OptionParser(usage=usage)
+    parser.add_option("-i", "--input", dest="input", metavar="FILE", help="Input file")
+    parser.add_option("-o", "--outputPath", metavar="PATH", dest="outputPath", default="output.svg", help="Output Path")
+    (options, args) = parser.parse_args()
 
-# Grid
-plt.grid(True, linestyle='--', color='grey', zorder=0)
+    young_gen_min = []
+    young_gen_max = []
+    young_gen_cur = []
 
-# Prepare x-axis data
-time = range(1, len(young_gen_min) + 1)
+    old_gen_min = []
+    old_gen_max = []
+    old_gen_cur = []
 
-plt.plot(time, young_gen_min, color=config.B_color_cycle[0],
-         label='New Generation Minimum Capacity', zorder=2)
-plt.plot(time, young_gen_max, color=config.B_color_cycle[1],
-         label='New Generation Maximum Capacity', zorder=2)
-plt.plot(time, young_gen_cur, color=config.B_color_cycle[3],
-         label='New Generation Current Capacity', zorder=2)
+    # Open input file 
+    with open(options.input, 'r') as inputFile:
+        # Initialize a flag to track whether the first line has been bypassed
+        first_line_skipped = False
 
-# Axis name
-plt.ylabel('Memory (GB)', ha="center")
-plt.xlabel('Time (s)')
+        # Iterate over each line in the file
+        for line in inputFile.readlines():
+            # Check if it's the first line
+            if not first_line_skipped:
+                # Set the flag to True to indicate that the first line has been bypassed
+                first_line_skipped = True
+                continue  # Skip processing this line and move to the next one
 
-# Legend
-plt.legend(loc='upper right', bbox_to_anchor=(1, 1.25), ncol=3)
+            # Split the line into individual columns based on whitespace
+            columns = line.strip().split()
 
-# Save figure
-plt.savefig('%s/young_gen.png' % options.outputPath, bbox_inches='tight')
+            young_gen_min.append(kb_to_gb(columns[0]))
+            young_gen_max.append(kb_to_gb(columns[1]))
+            young_gen_cur.append(kb_to_gb(columns[2]))
 
-#------------------------------------------------------------------------------
-# Old Generation Capacity
-#------------------------------------------------------------------------------
-fig, ax = plt.subplots(figsize=config.fullfigsize)
+            old_gen_min.append(kb_to_gb(columns[6]))
+            old_gen_max.append(kb_to_gb(columns[7]))
+            old_gen_cur.append(kb_to_gb(columns[8]))
 
-# Grid
-plt.grid(True, linestyle='--', color='grey', zorder=0)
+    time = range(1, len(young_gen_min) + 1)
 
-# Prepare x-axis data
-time = range(1, len(young_gen_min) + 1)
+    # Plot Young Generation Capacity
+    plot_capacity_data(time, young_gen_min, young_gen_max, young_gen_cur,
+                       'New Generation', f'{options.outputPath}/young_gen.png')
 
-plt.plot(time, old_gen_min, color=config.B_color_cycle[0],
-         label='Old Generation Minimum Capacity', zorder=2)
-plt.plot(time, old_gen_max, color=config.B_color_cycle[1],
-         label='Old Generation Maximum Capacity', zorder=2)
-plt.plot(time, old_gen_cur, color=config.B_color_cycle[3],
-         label='Old Generation Current Capacity', zorder=2)
+    # Plot Old Generation Capacity
+    plot_capacity_data(time, old_gen_min, old_gen_max, old_gen_cur,
+                       'Old Generation', f'{options.outputPath}/old_gen.png')
 
-# Axis name
-plt.ylabel('Memory (GB)', ha="center")
-plt.xlabel('Time (s)')
-
-# Legend
-plt.legend(loc='upper right', bbox_to_anchor=(1, 1.25), ncol=3)
-
-# Save figure
-plt.savefig('%s/old_gen.png' % options.outputPath, bbox_inches='tight')
+if __name__ == "__main__":
+    main()
